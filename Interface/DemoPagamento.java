b3f45c455ab5e4912110e5516adfae82dfaf8c65
Janieli Tainar <112900054+JanieliSilva@users.noreply.github.com>
2025-10-21 23:37:55 +0000

    add conteudo de interface

---
+++ Interface/DemoPagamento.java
@@ -0,0 +1,16 @@
+package Interface;
+
+public class DemoPagamento {
+    public static void main(String[] args) {
+        Fatura f = new Fatura(5.89, 5);
+        Assalariado a = new Assalariado("Lucas", "98639909", 9783.76);
+    System.out.println("Pagamentos da fatura" + f.getValorPagamento());
+
+        imprimeValorPagamento(a);
+        imprimeValorPagamento(f);
+    }
+
+    public static void imprimeValorPagamento(Pagavel pag) {
+        System.out.println(pag.getValorPagamento());
+    }
+}
